1ae8dd7233411cfcb758a3d5f4b4de3b62f036d1
zeusin <zeusin360@gmail.com>
2022-03-29 14:41:08 +0200

    Update Firefox_Preferences.sh

--- Firefox_Preferences.sh
+++ Firefox_Preferences.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 ###Cambia preferencias predeterminadas de firefox
 ########################
+ruta="/tmp/FirefoxUsers"
 
+echo -n "" > $ruta
 find /home/*/.mozilla/firefox/*.default* -maxdepth 0 -print0 | while read -d $'\0' file
 do
  if [ ! -f "$file/user.js" ]; then
   touch "$file/user.js"
-   echo "se crea $file ---------------------"
- fi
+  echo "se crea $file ---------------------"
+fi
+echo $file >> $ruta
 done
 
+
 CambiarPreferencia () {
-find /home/*/.mozilla/firefox/*.default* -maxdepth 0 -print0 | while read -d $'\0' file
+while read file
 do
  echo "estamos en $file parametro $1"
  Coincidencia=`grep -R 'user_pref('"$1"', *' "$file/user.js"`
@@ -23,14 +27,19 @@ do
   if [ $Condition == $2 ]; then
     echo "ok default"
   else
-    sed -i 's#'"$Coincidencia"'#user_pref('"$1"', '"$2"');#g' "$file/user.js"
+    sed -i 's^'"$Coincidencia"'^user_pref('"$1"', '"$2"');^g' "$file/user.js"
     echo "se cambia"
   fi
  fi
-done
+done < $ruta
 }
 
+#######################INTRODUCIR PARAMETROS A PARTIR DE AQUI#############################################################
+
 #Establece las paginas por defecto
-CambiarPreferencia '"browser.startup.homepage"' '"https://google.com|https://github.com"'
+CambiarPreferencia '"browser.startup.homepage"' '"http://intranet.tdnonline.com/PortalEmpleado|http://verne.tdnonline.com/produccion/index.jsp"'
 CambiarPreferencia '"browser.startup.page"' '1'
 #----------------------------------
+
+#########################################################################################################################
+rm $ruta
